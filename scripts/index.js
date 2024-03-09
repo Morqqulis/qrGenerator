c70ac4d27c98ad51e563303c9e0bcc70282cce33
Morqqulis <Morqqulis@gmail.com>
2024-03-09 20:14:32 +0400

    .

--- scripts/index.js
+++ scripts/index.js
@@ -4,11 +4,10 @@ const button = document.querySelector('.qr__button')
 const inputValue = document.querySelector('.qr__input')
 const qrImage = document.querySelector('.qr__image')
 
-const generateQR = url => {
-	qrImage.innerHTML = `
+const generateQR = url =>
+	(qrImage.innerHTML = `
         <img src="https://api.qrserver.com/v1/create-qr-code/?size=350x350&data=${url}">
-    `
-}
+    `)
 
 button.addEventListener('click', () => {
 	const url = inputValue.value.trim()
